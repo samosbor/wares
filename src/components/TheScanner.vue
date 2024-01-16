fc12dec310711a81f934ae91f727352d96000d95
sam <sam.osborne159@gmail.com>
2024-01-16 12:28:06 +0800

    first commit

---
+++ src/components/TheScanner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <h1>thescanner</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/ScanView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheScanner from '@/components/TheScanner.vue'
+import ScannedItems from '@/components/ScannedItems.vue'
+</script>
+
+<template>
+  <main>
+    <TheScanner />
+    <ScannedItems />
+  </main>
+</template>
